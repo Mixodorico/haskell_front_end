fde5430973b3995f7190ee9a4d9eaf9eb3176110
mixodorico <gilmour.david@hotmail.it>
2018-05-18 17:59:29 +0200

    Second commit

---
+++ test/ex1.go
@@ -0,0 +1,19 @@
+package main
+
+
+func sum (x, y int) int {
+	a :=x+y
+	b :=&a
+	x= *b
+return x
+}
+
+
+
+
+
+
+	
+
+
+
---
+++ test/ex3.go
@@ -0,0 +1,19 @@
+package main
+
+func ret () int{
+	pep:=true
+	return 5
+}
+var a,b,c= "ciao", 5+(2+8)*9 , ret()
+
+func ciao ( a int , d int) {
+	u :=4
+	u = a/d
+	ciao ("4",d)
+	func bin () bool {
+		k:= ret() > 9
+		return k
+	}
+	a = 789
+		
+}
