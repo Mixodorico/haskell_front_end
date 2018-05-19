724d2969119a14fe056ee54b13ca15ed34fdc100
mixodorico <gilmour.david@hotmail.it>
2018-05-19 15:52:50 +0200

    Grammatica parziale

--- test/ex1.go
+++ test/ex1.go
@@ -1,19 +1,17 @@
-package main
+package programma
 
 
 func sum (x, y int) int {
 	a :=x+y
 	b :=&a
+    var boolean = true
+    var e int 
+    var ciao char = 'a'
+    var stringa string = "LALR CBC marieeeeee"
+    var uno, due int = 5 , 6
+    //var fsf float = 2.0
+    var car = 'a' // senza tipo 
+    var a, b, c = 3, 4, "foo"
 	x= *b
 return x
 }
-
-
-
-
-
-
-	
-
-
-
