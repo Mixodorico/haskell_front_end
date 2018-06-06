49a29460ceb6b5301d305f30fdfc046564274c0d
mixodorico <gilmour.david@hotmail.it>
2018-06-06 09:34:13 +0200

    makefile sistemato e aggiunta cartella BUP

---
+++ tests/ex11.go
@@ -0,0 +1,13 @@
+package programma
+/*
+Scritto in base al type checker
+*/
+  
+  var i, j float
+  
+  var m[10] int
+  var n[1][1] int
+  
+  i = i % m[1]
+
+  //if 4.8 == true {}
---
+++ tests/prova.go
@@ -0,0 +1,19 @@
+package programma
+
+
+func sum (x, y int) int {
+    
+    asa, as := 5 , 6
+    x := 5
+    
+    if x = *5 ; x == 9 && y == a {
+        return a     
+    }
+    else 
+    {
+        return y 
+    }
+    
+    return x
+}
+
